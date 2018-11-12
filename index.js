a7e8eeb574977d3c5e09bae4582a5fd2199e0fa2
Jeff Lombard <jefflombardjr@gmail.com>
2018-11-11 20:27:02 -0500

    fixed schema error

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const mongoose = require('mongoose');
 const schema = require('./schema/index.js');
+const models = require('./models');
 
 const port = process.env.PORT || 3000;
 const app = express();
