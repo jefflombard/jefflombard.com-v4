60ba0f06effb6b9fdeb07f025c41bca7be686909
Jeff Lombard <jefflombardjr@gmail.com>
2018-11-11 20:31:41 -0500

    changed order

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const models = require('./models');
 const graphqlHTTP = require('express-graphql');
 const mongoose = require('mongoose');
-const models = require('./models');
 const schema = require('./schema/index.js');
 
 const port = process.env.PORT || 3000;
