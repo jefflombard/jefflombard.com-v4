45782ebf4e97c8f5b4b14bd096b488cbfaa1e457
Jeff Lombard <jefflombardjr@gmail.com>
2018-11-18 21:01:34 -0500

    added loading component

---
+++ src/src/components/Loading.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Loading = () => (
+  <div className="loading-container">
+    <div className="loading" />
+    <div className="loading" />
+  </div>
+);
+
+export default Loading;
