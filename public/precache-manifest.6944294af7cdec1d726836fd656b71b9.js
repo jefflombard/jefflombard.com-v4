f57a57bd03b2438238d9fe70fa4506df7934ab94
Jeff Lombard <jefflombardjr@gmail.com>
2018-11-20 22:13:41 -0500

    update

---
+++ public/precache-manifest.6944294af7cdec1d726836fd656b71b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b679f564f8349af8fed7",
+    "url": "/static/js/main.b679f564.chunk.js"
+  },
+  {
+    "revision": "d7cc55e38b9a817b37fc",
+    "url": "/static/js/1.d7cc55e3.chunk.js"
+  },
+  {
+    "revision": "b679f564f8349af8fed7",
+    "url": "/static/css/main.05857a42.chunk.css"
+  },
+  {
+    "revision": "b07ed4116d8d6b39dac646af80d4a2bd",
+    "url": "/index.html"
+  }
+];
